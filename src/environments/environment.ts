d914591b456142ee2a5451b065f4078fb8d174e6
Luksmdp <rivarola.mdq@gmail.com>
2024-10-27 20:33:39 -0300

    added keys for the api in environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,3 +8,11 @@ export const environment = {
       'x-rapidapi-key': '3c81862b04mshef79cda12bfe69fp124b3bjsn7da5465ea926'
     }
   };
+
+//   api keys:
+
+// 1fe7d2b86dmsh37c8b87897cd301p192317jsn03b40480d851
+
+// df7efde52amsh77cddb93fd71795p1b21fcjsndb5989adc208
+
+// 3c81862b04mshef79cda12bfe69fp124b3bjsn7da5465ea926
